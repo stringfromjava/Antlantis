eb87810f64b18797a1e1525237e42f39ce91874f
J4WLESS5 <josh103064@outlook.com>
2025-08-03 17:16:34 -0500

    added menu tweening

--- source/menus/MainMenuState.hx
+++ source/menus/MainMenuState.hx
@@ -1,5 +1,8 @@
 package menus;
 
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
 import ui.UIClickableText;
 import backend.util.FlixelUtil;
 import flixel.FlxG;
@@ -10,13 +13,13 @@ import flixel.util.FlxColor;
 class MainMenuState extends FlxState
 {
 	var titleText:FlxText;
-    var playText:UIClickableText;
+	var playText:UIClickableText;
 	var gameQuitText:UIClickableText;
 
-    override function create()
-    {
-        super.create();
-        FlixelUtil.playMenuMusic();
+	override function create()
+	{
+		super.create();
+		FlixelUtil.playMenuMusic();
 
 		titleText = new FlxText();
 		titleText.text = 'ANTLANTIS';
@@ -24,32 +27,40 @@ class MainMenuState extends FlxState
 		titleText.screenCenter(X);
 		add(titleText);
 
-        playText = new UIClickableText();
+		playText = new UIClickableText();
 		playText.text = 'Play';
-        playText.size = 64;
-		playText.screenCenter(XY);
-        playText.behavior.updateHoverBounds(playText.x, playText.y, playText.width, playText.height);
-        playText.behavior.onClick = () ->
-        {
-            FlxG.switchState(() -> new PlayState());
-        };
-        add(playText);
-        
+		playText.size = 64;
+		playText.screenCenter(X);
+		playText.y = FlxG.height;
+		playText.behavior.updateHoverBounds(playText.x, playText.y, playText.width, playText.height);
+		playText.behavior.onClick = () ->
+		{
+			FlxG.switchState(() -> new PlayState());
+		};
+		add(playText);
+
 		gameQuitText = new UIClickableText();
 		gameQuitText.text = 'Quit Game';
 		gameQuitText.size = 64;
 		gameQuitText.screenCenter(X);
 		gameQuitText.y = 400;
-        gameQuitText.behavior.updateHoverBounds(gameQuitText.x, gameQuitText.y, gameQuitText.width, gameQuitText.height);
-        gameQuitText.behavior.onClick = () ->
-        {
-            FlixelUtil.closeGame();
-        }
+		gameQuitText.behavior.updateHoverBounds(gameQuitText.x, gameQuitText.y, gameQuitText.width, gameQuitText.height);
+		gameQuitText.behavior.onClick = () ->
+		{
+			FlixelUtil.closeGame();
+		}
 		add(gameQuitText);
-    }
 
-    override function update(elapsed:Float) 
-    {
-        super.update(elapsed);
-    }
+		new FlxTimer().start(1, (_) ->
+		{
+			FlxTween.tween(playText, {y: (FlxG.height / 2) - (playText.height / 2)}, 0.75, {
+				ease: FlxEase.quadOut
+			});
+		}, 1);
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
 }
